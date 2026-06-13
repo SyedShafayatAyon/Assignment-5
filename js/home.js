3bca7567a447b50e01e17daa669f9d38116f2998
SyedShafayatAyon <syedshafayatayon@gmail.com>
2026-06-14 00:23:35 +0600

    all isuue modal completed

--- js/home.js
+++ js/home.js
@@ -12,25 +12,9 @@ const allIssue = () => {
 const displayAllIssue = (issues) => {
   const numOfIssue = document.getElementById("numOfIssue");
   numOfIssue.innerText = issues.length;
-  //   console.log(issues.length);
+
   const allIssueContainer = document.getElementById("allIssueContainer");
   allIssueContainer.innerHTML = "";
-
-  // {
-  //       "id": 1,
-  //       "title": "Fix navigation menu on mobile devices",
-  //       "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-  //       "status": "open",
-  //       "labels": [
-  //         "bug",
-  //         "help wanted"
-  //       ],
-  //       "priority": "high",
-  //       "author": "john_doe",
-  //       "assignee": "jane_smith",
-  //       "createdAt": "2024-01-15T10:30:00Z",
-  //       "updatedAt": "2024-01-15T10:30:00Z"
-  //     }
 
   issues.forEach((issue) => {
     const borderColor =
@@ -43,12 +27,10 @@ const displayAllIssue = (issues) => {
         ? `<img src="B13-A5-Github-Issue-Tracker/assets/Open-Status.png" alt="">`
         : `<img src="B13-A5-Github-Issue-Tracker/assets/Closed-Status.png" alt="">`;
 
-    issue.labels.forEach((item) => console.log(item));
-
     const issueCard = document.createElement("div");
     issueCard.innerHTML = `
     
-    <div class="p-4 bg-white rounded-md h-full shadow-[0px_1px_6px_rgba(0,0,0,0.08)] ${borderColor}">
+    <div onclick=loadIssueDetail(${issue.id}) class="p-4 bg-white rounded-md h-full shadow-[0px_1px_6px_rgba(0,0,0,0.08)] ${borderColor}">
                     <div class="  space-y-3 pb-4">
                         <div class="flex justify-between items-center">
 
@@ -56,10 +38,10 @@ const displayAllIssue = (issues) => {
 
                           ${
                             issue.priority === "high"
-                              ? `<button class="bg-[#FEECEC] px-7 py-2 rounded-[100px] text-[#EF4444]  text-xs font-medium ">${issue.priority.toUpperCase()}</button>`
+                              ? `<div class="bg-[#FEECEC] px-7 py-2 rounded-[100px] text-[#EF4444]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
                               : issue.priority === "medium"
-                                ? `<button class="bg-[#FFF6D1] px-7 py-2 rounded-[100px] text-[#F59E0B]  text-xs font-medium ">${issue.priority.toUpperCase()}</button>`
-                                : `<button class="bg-[#EEEFF2] px-7 py-2 rounded-[100px] text-[#9CA3AF]  text-xs font-medium ">${issue.priority.toUpperCase()}</button>`
+                                ? `<div class="bg-[#FFF6D1] px-7 py-2 rounded-[100px] text-[#F59E0B]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
+                                : `<div class="bg-[#EEEFF2] px-7 py-2 rounded-[100px] text-[#9CA3AF]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
                           }
                             
 
@@ -70,32 +52,32 @@ const displayAllIssue = (issues) => {
                         <div class="flex gap-1 flex-wrap">
                         ${issue.labels.map((label) =>
                           label === "bug"
-                            ? ` <button
+                            ? ` <div
                                 class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FECACA] rounded-[100px] text-[#EF4444] text-xs font-medium">
                                 <img src="B13-A5-Github-Issue-Tracker/assets/BugDroid.png" alt="">BUG
-                            </button> `
+                            </div> `
                             : label === "help wanted"
-                              ? `<button
+                              ? `<div
                                 class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FDE68A] rounded-[100px] text-[#D97706] text-xs font-medium ">
                                 <img src="B13-A5-Github-Issue-Tracker/assets/Lifebuoy.png" alt="">HELP WANTED
-                            </button>`
+                            </div>`
                               : label == "enhancement"
-                                ? `<button
+                                ? `<div
                                 class="flex justify-center items-center gap-1 px-4 py-1 bg-[#DEFCE8] rounded-[100px] text-[#00A96E] text-xs font-medium ">
                                 <img src="B13-A5-Github-Issue-Tracker/assets/Sparkle.png" alt="">
                                 ENHANCEMENT  
-                            </button>`
+                            </div>`
                                 : label == "documentation"
-                                  ? `<button
+                                  ? `<div
                                 class="flex justify-center items-center gap-1 px-4 py-1 bg-slate-300 rounded-[100px] text-[#] text-xs font-medium ">
                                 <img src="B13-A5-Github-Issue-Tracker/assets/folder.png" alt="">
                                 DOCUMENTATION
-                            </button>`
-                                  : `<button
+                            </div>`
+                                  : `<div
                                 class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FDE68A] rounded-[100px] text-[#D97706] text-xs font-medium ">
                                 <img src="B13-A5-Github-Issue-Tracker/assets/Lifebuoy.png" alt="">
                                 GOOD FIRST ISSUE
-                            </button>`,
+                            </div>`,
                         )}
                         </div>
                         <hr class="-mx-4 border-0 border-t-2 border-[#E4E4E7]">
@@ -113,3 +95,84 @@ const displayAllIssue = (issues) => {
 };
 
 allIssue();
+
+const loadIssueDetail = (issueId) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => displayIssueDetail(json.data));
+};
+
+const displayIssueDetail = (issue) => {
+  const bgColor = issue.status === "open" ? "bg-[#00A96E]" : "bg-[#A855F7]";
+
+  const issueDetails = document.getElementById("issueDetails");
+  issueDetails.innerHTML = `
+  <div>
+          <h3 class="text-2xl font-bold">${issue.title}</h3>
+
+          <div class="flex gap-6 items-center  text-xs font-medium mb-6">
+              <div class="px-2 py-[6px] border-none rounded-full text-white ${bgColor}">${issue.status}</div>
+              <ol class="flex gap-6 list-disc text-[#64748B] text-xs">
+                  <li> ${issue.assignee === "" ? "Unassigned" : `Opened by ${issue.assignee}`}</li>
+                  <li>${issue.createdAt.split("T")[0]}</li>
+              </ol>
+          </div>
+
+          <div class="flex gap-1 flex-wrap mb-6">
+                        ${issue.labels
+                          .map((label) =>
+                            label === "bug"
+                              ? ` <div
+                                class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FECACA] rounded-[100px] text-[#EF4444] text-xs font-medium">
+                                <img src="B13-A5-Github-Issue-Tracker/assets/BugDroid.png" alt="">BUG
+                            </div> `
+                              : label === "help wanted"
+                                ? `<div
+                                class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FDE68A] rounded-[100px] text-[#D97706] text-xs font-medium ">
+                                <img src="B13-A5-Github-Issue-Tracker/assets/Lifebuoy.png" alt="">HELP WANTED
+                            </div>`
+                                : label == "enhancement"
+                                  ? `<div
+                                class="flex justify-center items-center gap-1 px-4 py-1 bg-[#DEFCE8] rounded-[100px] text-[#00A96E] text-xs font-medium ">
+                                <img src="B13-A5-Github-Issue-Tracker/assets/Sparkle.png" alt="">
+                                ENHANCEMENT  
+                            </div>`
+                                  : label == "documentation"
+                                    ? `<div
+                                class="flex justify-center items-center gap-1 px-4 py-1 bg-slate-300 rounded-[100px] text-[#] text-xs font-medium ">
+                                <img src="B13-A5-Github-Issue-Tracker/assets/folder.png" alt="">
+                                DOCUMENTATION
+                            </div>`
+                                    : `<div
+                                class="flex justify-center items-center gap-1 px-4 py-1 bg-[#FDE68A] rounded-[100px] text-[#D97706] text-xs font-medium ">
+                                <img src="B13-A5-Github-Issue-Tracker/assets/Lifebuoy.png" alt="">
+                                GOOD FIRST ISSUE
+                            </div>`,
+                          )
+                          .join("")}
+                        </div>
+
+          <p class="text-[#64748B]">${issue.description}</p>
+
+          <div class="bg-[#F8FAFC] p-4 flex gap-50">
+              <div>
+                  <h5 class="text-[#64748B]">Assignee:</h5>
+                  <p class="font-semibold">${issue.assignee === "" ? "Unassigned" : issue.assignee}</p>
+              </div>
+              <div>
+                  <h5 class="text-[#64748B]">Priority:</h5>
+                  ${
+                    issue.priority === "high"
+                      ? `<div class="bg-[#FEECEC] px-4 py-2 rounded-[100px] text-[#EF4444]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
+                      : issue.priority === "medium"
+                        ? `<div class="bg-[#FFF6D1] px-4 py-2 rounded-[100px] text-[#F59E0B]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
+                        : `<div class="bg-[#EEEFF2] px-4 py-2 rounded-[100px] text-[#9CA3AF]  text-xs font-medium ">${issue.priority.toUpperCase()}</div>`
+                  }
+              </div>
+
+               </div>
+           </div>
+  `;
+  document.getElementById("issueModal").showModal();
+};
